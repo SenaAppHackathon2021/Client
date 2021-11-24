eca4d4788c388e36279a07c45a5ed50994a806b0
정민혁 <jlssteeve1@gmail.com>
2021-11-24 20:49:15 +0900

    [ADD] BaseActivity

--- app/src/main/java/com/app/reart/base/BaseActivity.kt
+++ app/src/main/java/com/app/reart/base/BaseActivity.kt
@@ -2,11 +2,16 @@ package com.app.reart.base
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import com.app.reart.R
 
-class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity<T : ViewDataBinding>(@LayoutRes private val layoutResId: Int) : AppCompatActivity() {
+    lateinit var binding : T
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_base)
+        binding = DataBindingUtil.setContentView(this, layoutResId)
     }
 }

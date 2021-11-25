8c333f700b4faa12ea53341b33d9e415fa7da020
양태웅 <didxodnd0422@gmail.com>
2021-11-26 04:46:51 +0900

    [UPDATE] Apply viewmodels and display dummy data

--- app/src/main/java/com/app/reart/view/fragments/artboard/ArtBoardFragment.kt
+++ app/src/main/java/com/app/reart/view/fragments/artboard/ArtBoardFragment.kt
@@ -6,20 +6,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.app.reart.R
+import com.app.reart.adapter.BoardListAdapter
 import com.app.reart.databinding.FragmentArtBoardBinding
+import com.app.reart.view.fragments.home.HomeViewModel
 
 class ArtBoardFragment : Fragment() {
     private lateinit var binding: FragmentArtBoardBinding
+    private lateinit var viewModel: ArtBoardViewModel
+    private val postAdapter: BoardListAdapter by lazy { BoardListAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_art_board, container, false)
+        viewModel = ViewModelProvider(requireActivity())[ArtBoardViewModel::class.java]
+        binding.postRecyclerView.adapter = postAdapter
 
+        viewModel.getPosts()
 
+        observeData()
 
         return binding.root
+    }
+
+    private fun observeData() {
+        viewModel.postResponse.observe(this, { postList ->
+            postAdapter.setData(postList)
+        })
     }
 }
--- app/src/main/java/com/app/reart/view/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/app/reart/view/fragments/home/HomeFragment.kt
@@ -6,12 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.viewpager2.widget.ViewPager2
+import androidx.lifecycle.ViewModelProvider
 import com.app.reart.R
+import com.app.reart.adapter.BoardListAdapter
 import com.app.reart.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var viewModel: HomeViewModel
+    private val postAdapter: BoardListAdapter by lazy { BoardListAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,8 +22,19 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        viewModel = ViewModelProvider(requireActivity())[HomeViewModel::class.java]
+        binding.postRecyclerView.adapter = postAdapter
 
+        viewModel.getPosts()
+
+        observeData()
 
         return binding.root
+    }
+
+    private fun observeData() {
+        viewModel.postResponse.observe(this, { postList ->
+            postAdapter.setData(postList)
+        })
     }
 }

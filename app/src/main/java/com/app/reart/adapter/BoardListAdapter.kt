fccb741e108dc8f01fe95dfe0069f5de3bc25494
양태웅 <didxodnd0422@gmail.com>
2021-11-26 04:48:34 +0900

    [UPDATE] Apply changed model name

--- app/src/main/java/com/app/reart/adapter/BoardListAdapter.kt
+++ app/src/main/java/com/app/reart/adapter/BoardListAdapter.kt
@@ -3,15 +3,17 @@ package com.app.reart.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.app.reart.R
 import com.app.reart.databinding.ItemArtBoardBinding
-import com.app.reart.model.ArtBoardItem
+import com.app.reart.model.BoardItem
 
 class BoardListAdapter: RecyclerView.Adapter<BoardListAdapter.BoardListViewHolder>() {
-    var postList: ArrayList<ArtBoardItem> = arrayListOf()
+    var postList: ArrayList<BoardItem> = arrayListOf()
 
     class BoardListViewHolder(private var binding: ItemArtBoardBinding): RecyclerView.ViewHolder(binding.root) {
-        fun bind(item: ArtBoardItem) {
-
+        fun bind(item: BoardItem) {
+            // TODO : Update image loading logic
+            binding.imageThumb.setImageResource(item.thumbnailSrc ?: R.drawable.re_art_logo)
             binding.textTitle.text = item.title
             binding.textAuthor.text = "요청자 : ${item.author}"
             binding.textDate.text = item.date
@@ -35,7 +37,7 @@ class BoardListAdapter: RecyclerView.Adapter<BoardListAdapter.BoardListViewHolde
 
     override fun getItemCount(): Int = postList.size
 
-    fun setData(newPostList: List<ArtBoardItem>) {
+    fun setData(newPostList: List<BoardItem>) {
         postList = ArrayList(newPostList)
 
         notifyDataSetChanged()

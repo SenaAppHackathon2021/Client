7426cf930008e479a9614697e1ba58f5dcdf6599
양태웅 <didxodnd0422@gmail.com>
2021-11-26 04:43:01 +0900

    [ADD] Apply Splash Theme

--- app/src/main/java/com/app/reart/view/MainActivity.kt
+++ app/src/main/java/com/app/reart/view/MainActivity.kt
@@ -18,7 +18,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_Reart)
         super.onCreate(savedInstanceState)
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment

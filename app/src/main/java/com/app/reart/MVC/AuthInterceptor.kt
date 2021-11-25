37aedf2f5a2d4c4078ebf8cd58a6fa099e8deda7
JJ7224 <63018169+JJ7224@users.noreply.github.com>
2021-11-25 21:55:38 +0900

    [ADD] : Authinterceptor.kt

---
+++ app/src/main/java/com/app/reart/MVC/AuthInterceptor.kt
@@ -0,0 +1,15 @@
+package com.app.reart.MVC
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        var req = chain
+            .request()
+            .newBuilder()
+            .addHeader("Authorization", "Bearer " + App.prefs.token ?: "")
+            .build()
+        return chain.proceed(req)
+    }
+}

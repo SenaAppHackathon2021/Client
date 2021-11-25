afcc335b29a3965114fb1c19d1bf02af7dee28d4
양태웅 <didxodnd0422@gmail.com>
2021-11-25 23:32:29 +0900

    [UPDATE] Integrate NavigationComponent wih BottomNavigationView

--- app/src/main/java/com/app/reart/view/MainActivity.kt
+++ app/src/main/java/com/app/reart/view/MainActivity.kt
@@ -2,13 +2,41 @@ package com.app.reart.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.app.reart.R
+import com.app.reart.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
 
 
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.artBoardFragment,
+                R.id.materialBoardFragment,
+                R.id.myPageFragment
+            )
+        )
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
+    override fun onNavigateUp(): Boolean = navController.navigateUp() || super.onNavigateUp()
 }

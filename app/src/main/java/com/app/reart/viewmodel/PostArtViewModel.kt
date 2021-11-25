349769f90eae01e75e1160f86b232ccca35bf62c
양태웅 <didxodnd0422@gmail.com>
2021-11-25 18:15:16 +0900

    [ADD] uploadPost function

--- app/src/main/java/com/app/reart/viewmodel/PostArtViewModel.kt
+++ app/src/main/java/com/app/reart/viewmodel/PostArtViewModel.kt
@@ -1,10 +1,17 @@
 package com.app.reart.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import android.net.Uri
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.app.reart.model.ArtPost
+import com.app.reart.model.Picture
+import com.google.gson.Gson
 
 class PostArtViewModel: ViewModel() {
+    var photoBeforeUri: Uri? = null
+    var photoBeforeBase64: String = ""
+    var photoAfterUri: Uri? = null
+    var photoAfterBase64: String = ""
     val supporterList = ArrayList<String>(arrayListOf())
 
     fun addSupporter(supporter: String) {
@@ -13,5 +20,11 @@ class PostArtViewModel: ViewModel() {
 
     fun removeSupporter() {
         supporterList.clear()
+    }
+
+    fun uploadPost(artPost: ArtPost) {
+        val post = Gson().toJson(artPost)
+
+        Log.d("uploadPost", "onCreateView: $post")
     }
 }
